6863053c6f49d84ac19febfc3a211cce1197a0e5
Todd Carlson <todd.carlson3030@me.com>
2020-05-04 15:30:04 -0600

    Added details page and set up Reach router

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -4,7 +4,7 @@ import Results from './Results'
 import useDropdown from './useDropdown'
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Denver, CO") 
+    const [location, setLocation] = useState("San Francisco, CA") 
     const [breeds, setBreeds] = useState([]) 
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS)
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds) 
