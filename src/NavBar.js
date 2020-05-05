838f004458e620bc948097a84aa52a5c1297faa3
Todd Carlson <todd.carlson3030@me.com>
2020-05-05 16:02:34 -0600

    added nav-bar

---
+++ src/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from '@reach/router'
+import { css, keyframes } from '@emotion/core' 
+
+
+const NavBar = () => (
+    <header 
+    css={css`
+        background-color: #3333; 
+        padding: 15px;
+    `}
+    >
+        <Link to="/">Adopt Me!</Link>
+        <span css={css` 
+        font-size: 60px;  
+        `}
+        role="img" aria-label="logo">🐶</span>
+    </header>
+) 
+
+export default NavBar
